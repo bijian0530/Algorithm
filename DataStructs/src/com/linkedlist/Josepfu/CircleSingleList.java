5aa447ea00172d0a0f0c06e12ae5b034b0222109
bijian0530 <bj5201314>
2021-03-01 21:45:39 +0800

    测试

---
+++ DataStructs/src/com/linkedlist/Josepfu/CircleSingleList.java
@@ -0,0 +1,57 @@
+package com.linkedlist.Josepfu;
+
+public class CircleSingleList {
+    private Boy first = null;
+
+    //构建环形链表
+    public void addBoy(int nums){
+        if (nums < 1){
+            System.out.println("输入的nums值不正确");
+            return;
+        }
+        //构建辅助指针
+        Boy curboy = new Boy(-1);
+        for (int i = 0; i <= nums; i++) {
+            Boy boy = new Boy(i);
+            if(i == 1){
+                first = boy;
+                first.setNext(first);
+                curboy = first;
+            }else {
+                curboy.setNext(boy);
+                boy.setNext(first);
+                curboy = boy;
+            }
+        }
+    }
+
+    //遍历当前环形链表
+    public void showBoy(){
+        if (first == null){
+            System.out.println("没有数据");
+            return;
+        }
+
+        Boy curBoy = first;
+        while (true){
+            System.out.println("小孩的编号"+curBoy.getNo()+".");
+            if (curBoy.getNext() == first){
+                break;
+            }
+            curBoy = curBoy.getNext();
+        }
+    }
+
+    //根据用户的输入，计算出小孩的出圈顺序
+    public void countBoy(int startNo,int countNum,int nums){
+        if (first == null || startNo < 1 ||startNo >nums){
+            System.out.println("参数输入错误");
+            return;
+        }
+
+
+
+    }
+
+
+}
---
+++ DataStructs/src/com/linkedlist/Josepfu/Test.java
@@ -0,0 +1,9 @@
+package com.linkedlist.Josepfu;
+
+public class Test {
+    public static void main(String[] args) {
+        CircleSingleList circleSingleList = new CircleSingleList();
+        circleSingleList.addBoy(5);
+        circleSingleList.showBoy();
+    }
+}
